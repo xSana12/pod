8dabad8c9cc45318dfb86fe34369136a4e910192
xSana12 <57547849+xSana12@users.noreply.github.com>
2019-12-02 13:02:29 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -191,7 +191,7 @@ function claim($token)
     }
      function ride($token)
     {
-    $data = '{"promo_code":"COBAINGOJEK"}';    
+    $data = '{"promo_code":"WADAWGOJEK"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
     if ($claim['success'] == 1)
         {
